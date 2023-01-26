cb7cd6f79bb3558a457e98ad16d3d2fb20a85704
mabbasi17 <m.abbasi17@gmail.com>
2023-01-26 07:43:54 +0300

    menu ok

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,28 +1,33 @@
 import { Button, Menu, MenuItem } from '@mui/material';
 import Popover from '@mui/material/Popover';
-import PopupState from 'material-ui-popup-state';
+
 import React from 'react';
+import {
+  usePopupState,
+  bindTrigger,
+  bindMenu,
+} from 'material-ui-popup-state/hooks'
+import PopupState from 'material-ui-popup-state';
 
 
 interface Props {}
 
 function Navbar(props: Props) {
     const { } = props
-
     return (
     <>
       <PopupState variant="popover" popupId="demo-popup-menu">
   {(popupState) => (
-    // <React.Fragment>
-    //   <Button variant="contained" {...bindTrigger(popupState)}>
-    //     Dashboard
-    //   </Button>
-    //   <Menu {...bindMenu(popupState)}>
-    //     <MenuItem onClick={popupState.close}>Profile</MenuItem>
-    //     <MenuItem onClick={popupState.close}>My account</MenuItem>
-    //     <MenuItem onClick={popupState.close}>Logout</MenuItem>
-    //   </Menu>
-    // </React.Fragment>
+    <React.Fragment>
+      <Button variant="contained" {...bindTrigger(popupState)}>
+        Dashboard
+      </Button>
+      <Menu {...bindMenu(popupState)}>
+        <MenuItem onClick={popupState.close}>Profile</MenuItem>
+        <MenuItem onClick={popupState.close}>My account</MenuItem>
+        <MenuItem onClick={popupState.close}>Logout</MenuItem>
+      </Menu>
+    </React.Fragment>
   )}
 </PopupState>
         
